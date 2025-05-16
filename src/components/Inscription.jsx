114f65d2e4aa5efec880ac85c38ac3ce2641ed90
Eligenaut <brunobrayane@gmail.com>
2025-05-17 00:13:34 +0300

    Modification Inscription.jsx

--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { registerUser } from '../services/api'; 
 
 function Inscription({ onSwitch }) {
   const [formData, setFormData] = useState({
